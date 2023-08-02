561eeecf87aac36d2bdf25d1b37eb6eb230182fd
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2023-08-02 05:02:00 +0000

    Release 0.0.24

--- src/seam/core/client_wrapper.py
+++ src/seam/core/client_wrapper.py
@@ -13,7 +13,7 @@ class BaseClientWrapper:
         headers: typing.Dict[str, str] = {
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "fern-seam",
-            "X-Fern-SDK-Version": "0.0.23",
+            "X-Fern-SDK-Version": "0.0.24",
         }
         headers["seam-workspace"] = self.api_key
         return headers

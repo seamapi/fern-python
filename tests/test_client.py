4c4c4a2db7aa091817d4cd9dd54f5d683d251597
dsinghvi <deep@usebirch.com>
2023-08-07 12:05:43 -0400

    add some tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,6 +1,13 @@
-import pytest
 
-# Get started with writing tests with pytest at https://docs.pytest.org
-@pytest.mark.skip(reason="Unimplemented")
-def test_client() -> None:
-    assert True == True
+from seam.client import Seam
+import os
+
+seam_client = Seam(api_key="YOUR_API_KEY")
+
+def test_get_devices() -> None:
+    devices_response = seam_client.devices.list()
+    print(f"First device id is {devices_response.devices[0].device_id}")
+
+def test_create_access_code() -> None: 
+    create_response = seam_client.access_codes.create(device_id="138520f8-4b7f-4c04-9808-46de5303f0bd")
+    print(f"Access code status is {create_response.access_code.status}")

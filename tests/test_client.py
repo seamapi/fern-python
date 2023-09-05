73d4f381016b4423046d80891f795e073faa32bd
Severin Ibarluzea <seveibar@gmail.com>
2023-09-05 16:30:06 -0700

    more failing tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,10 +1,21 @@
 from seamapi import Seam
-import os
+
 
 def test_get_devices(seam: Seam):
     devices_response = seam.devices.list()
-    print(f"First device id is {devices_response[0].device_id}")
+    assert devices_response[0].device_id
 
-# def test_create_access_code(seam: Seam): 
-#     create_response = seam.access_codes.create(device_id="august_device_1")
-#     print(f"Access code status is {create_response.status}")
+
+def test_create_access_code(seam: Seam):
+    access_code = seam.access_codes.create(device_id="august_device_1", code="1234")
+    assert access_code.status == "pending"
+    assert access_code.code == "1234"
+
+
+def test_list_access_codes(seam: Seam):
+    access_codes = seam.access_codes.list(device_id="august_device_1")
+    assert len(access_codes) == 0
+
+    seam.access_codes.create(device_id="august_device_id")
+    access_codes = seam.access_codes.list(device_id="august_device_1")
+    assert len(access_codes) == 0

f692608ce7c43d0c028591d8d7b9fb9f8fa99f6b
seveibar <seveibar@gmail.com>
2023-09-04 19:33:40 -0700

    example test running against seam.vc

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,6 +1,10 @@
-import pytest
+from seamapi import Seam
+import os
 
-# Get started with writing tests with pytest at https://docs.pytest.org
-@pytest.mark.skip(reason="Unimplemented")
-def test_client() -> None:
-    assert True == True
+def test_get_devices(seam: Seam):
+    devices_response = seam.devices.list()
+    print(f"First device id is {devices_response[0].device_id}")
+
+# def test_create_access_code(seam: Seam): 
+#     create_response = seam.access_codes.create(device_id="august_device_1")
+#     print(f"Access code status is {create_response.status}")
